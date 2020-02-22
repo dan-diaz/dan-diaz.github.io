3620493738c29185341561cb9b2d2503841333de
Daniel Diaz <ddiaz1982@gmail.com>
2020-02-22 08:41:49 -0500

    build

---
+++ precache-manifest.dbc95d7c0051e4e7266f69175650e4fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9183e8c7b46ebbc98e1d228097ca9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b762686738f50d3baa",
+    "url": "/static/css/main.c7035f25.chunk.css"
+  },
+  {
+    "revision": "e156cfbaf86bf4547358",
+    "url": "/static/js/2.ba4771d9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ba4771d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b762686738f50d3baa",
+    "url": "/static/js/main.24c90c33.chunk.js"
+  },
+  {
+    "revision": "f8c19a4668beb9106046",
+    "url": "/static/js/runtime-main.0bc3bd93.js"
+  }
+]);
